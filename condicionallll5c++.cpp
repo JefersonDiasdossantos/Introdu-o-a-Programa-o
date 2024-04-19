e28d6c07b5847517f74236b36dfc4c454f69953a
JefersonDiasdossantos <161830985+JefersonDiasdossantos@users.noreply.github.com>
2024-04-19 08:17:21 -0300

    Add files via upload

---
+++ condicionallll5c++.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <clocale>
+
+using namespace std;
+int main () {
+	int ano, resto;
+	setlocale(LC_ALL,"portuguese");
+   
+	cin>>ano;
+	resto + ano%4;
+	if (resto==0){ 
+	cout<<"ano bissexto";
+
+	cout<<"nao é bissexto";
+	}
+	
+	
+	return 0;
+}
+	
+	
+	
+	
+
+
+
