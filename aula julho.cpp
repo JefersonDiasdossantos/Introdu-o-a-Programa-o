9fb7b6029baa9c5dda15bec8c5beee162817f747
JefersonDiasdossantos <161830985+JefersonDiasdossantos@users.noreply.github.com>
2024-07-12 08:48:19 -0300

    Add files via upload

---
+++ aula julho.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+	int idade[]= {23,22,21,26,18,43,13,12,21,4};
+	
+for(int i=9 ; i>=0; i--){
+
+cout<<i<<" "<<idade[i]<<endl;
+}
+
+
+
+
+
+
+
+
+
+
+return 0; 
+}
+
+
